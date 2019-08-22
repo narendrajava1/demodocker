a946dc5df7e7dceb6d9300eeca0f4034c757695b
narendrajavatech <knarendra585@gmail.com>
2019-08-22 22:51:01 +0530

    added modelclass and jpa and lombok dependency

--- src/main/java/com/example/demodocker/model/Animal.java
+++ src/main/java/com/example/demodocker/model/Animal.java
@@ -1,18 +1,27 @@
 package com.example.demodocker.model;
 
+import java.time.ZonedDateTime;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
 @MappedSuperclass
 @Data
-public class Employee {
+public class Animal {
 	@Id
 	@GeneratedValue
 	private long id;
 	private String name;
+	@CreationTimestamp
+	private ZonedDateTime createdDateTime;
+	@UpdateTimestamp
+	private ZonedDateTime updatedDateTime;
 
 //  @OneToMany(cascade = CascadeType.ALL)
 //  private List<Phone> phones;
