ff6bfeed61e681bd1a9ad42986eaeaa69081433c
narendrajava1 <30337207+narendrajava1@users.noreply.github.com>
2019-09-26 16:22:52 +0530

    Update DemoDockerApplicationTests.java

--- src/test/java/com/example/demodocker/DemoDockerApplicationTests.java
+++ src/test/java/com/example/demodocker/DemoDockerApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.demodocker;
+// package com.example.demodocker;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoDockerApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class DemoDockerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
